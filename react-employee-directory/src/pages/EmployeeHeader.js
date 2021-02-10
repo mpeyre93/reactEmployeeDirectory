145f16463b58db088aeddcf438236ea7a9058ae9
Marlee Eyre <marlee.eyre@gmail.com>
2021-02-09 21:39:14 -0500

    stlye added to app header

---
+++ react-employee-directory/src/pages/EmployeeHeader.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import "./style.css";
+
+function EmployeeHeader() {
+    return (
+<div className = "wrapper">
+    <div className = "title">
+Employee Information
+    </div>
+    <br></br>
+</div>
+
+    );
+}
+
+export default EmployeeHeader;
