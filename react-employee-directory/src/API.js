8c235f643519770f8af47eb733d19874bfc79358
Marlee Eyre <marlee.eyre@gmail.com>
2021-02-18 17:53:53 -0500

    removed un-needed api key

--- react-employee-directory/src/API.js
+++ react-employee-directory/src/API.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-const BASEURL = "https://randomuser.me/api/?inc=gender,name,email,picture";
-const APIKEY = "3T8L-O2DB-J0RU-8MY4";
-
-// Export an object with a "search" method that searches URL for the passed query
 export default {
-  search: function(query) {
-    return axios.get(BASEURL + query + APIKEY);
-  }
+    //Get random users
+    getUsers: function () {
+        return axios.get("https://randomuser.me/api/?results=300&nat=us");
+    }
 };
