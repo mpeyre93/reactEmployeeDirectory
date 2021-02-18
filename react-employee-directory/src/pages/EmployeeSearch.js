a0c2b1e2ecdd4ab31d51fc11794326e024ab9781
Marlee Eyre <marlee.eyre@gmail.com>
2021-02-18 18:53:42 -0500

    search added to header

--- react-employee-directory/src/pages/EmployeeSearch.js
+++ react-employee-directory/src/pages/EmployeeSearch.js
@@ -1,25 +1,42 @@
-import React from "react";
+import React from "react"
+import "./employeeStyle.css";
 
-function EmployeeSearch (props) {
-  return (
-    <form>
-      <div className="form-group">
-        <label htmlFor="search">Search:</label>
-        <input
-          onChange={props.handleInputChange}
-          value={props.search}
-          name="search"
-          type="text"
-          className="form-control"
-          placeholder="Search for a Gif"
-          id="search"
-        />
-        <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">
-          Search Employee
-        </button>
-      </div>
-    </form>
-  );
+function EmployeeSearch(props) {
+
+    return (
+        <div className="container-fluid">
+            <header className="z-depth-3 col s12" >
+                <div className="row">
+                    {/* header */}
+                    <div className="col s6">
+                        <h3 className="right-align headerText">Employee Information Directory</h3>
+                        {/* Use to narrow your search */}
+                    </div>
+                    <div className="col s6 ">
+                        <div className="col s6 inputAndButton right-align ">
+                            <input
+                                onChange={props.handleInputChange}
+                                value={props.value}
+                                id="employees"
+                                type="text"
+                                name="search"
+                                list="employee"
+                                className=" inputBox  "
+                                placeholder="Search by name" />
+                        </div>
+                        <div className="col m4">
+                            <button
+                                type="submit"
+                                value=""
+                                className=" btn z-depth-2 waves-effect  searchBttn"
+                                onClick={props.handleSearch} >Search</button>
+                        </div>
+                    </div>
+                    {/* header */}
+                </div>
+            </header>
+        </div >
+    )
 }
 
-export default EmployeeSearch;
+export default EmployeeSearch
