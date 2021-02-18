859565ea8008c360062af8fa0af594f5c973dffd
Marlee Eyre <marlee.eyre@gmail.com>
2021-02-18 18:41:31 -0500

    APi imported correctly

--- react-employee-directory/src/pages/EmployeeResults.js
+++ react-employee-directory/src/pages/EmployeeResults.js
@@ -1,120 +1,106 @@
-import React from "react";
-import "./results.css";
-import EmployeeInfo from './EmployeeInfo';
+import API from "../API";
+import React, { Component } from "react"
+import EmployeeSearch from "./EmployeeSearch"
+import EmployeeInfo from "./EmployeeInfo"
+import "./employeeStyle.css";
 
-class EmployeeResults extends React.Component {
+class EmployeeResults extends Component {
 
+    // Setting the component's initial state
+    //search starts as an empty string
+    //employess and filteredEmployess are empty arrays because that it's the structure of the data we'll be working with
+    //order it's to reference the order that the employees are. By defaltu they come randonly and the first click will trigger then to be in asc order
     state = {
-        alphabetical: true,
-        ascending: true,
-        sortedEmployees: [],
-        employees: []
-    }
+        search: "",
+        employees: [],
+        filteredEmployees: [],
+        order: ""
+    };
 
+    //initialization, when page is first loaded
     componentDidMount() {
-        if (this.state.sortedEmployees.length < 1) {
-            this.setState({
-                sortedEmployees: this.props.empList
-            })
-        }
+        API.getUsers().then(res => this.setState({
+            employees: res.data.results,
+            filteredEmployees: res.data.results
+        })).catch(err => console.log(err))
     }
 
-    componentDidUpdate(prevProps) {
-        if (this.props.empList !== prevProps.empList) {
+    //if "name" is clicked employee shown by asc/desc order
+    sortByName = () => {
+        const filtereds = this.state.filteredEmployees;
+        if (this.state.order === "asc") {
+            const sorteds = filtereds.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1)
+            console.log(sorteds)
             this.setState({
-                sortedEmployees: this.props.empList
-            })
-        }
-    }
-
-    sortName = () => {
-        let sortEmp = [];
-        if (this.state.alphabetical) {
-            sortEmp = this.props.empList.sort((a, b) => {
-                var nameA = a.name.last.toLowerCase(), nameB = b.name.last.toLowerCase();
-                if (nameA < nameB)
-                    return -1
-                if (nameA > nameB)
-                    return 1
-                return 0
+                filteredEmployees: sorteds,
+                order: "desc"
             })
         } else {
-            sortEmp = this.props.empList.sort((a, b) => {
-                var nameA = a.name.last.toLowerCase(), nameB = b.name.last.toLowerCase();
-                if (nameA > nameB)
-                    return -1
-                if (nameA < nameB)
-                    return 1
-                return 0
+            const sorteds = filtereds.sort((a, b) => (a.name.first > b.name.first) ? -1 : 1)
+            console.log(sorteds)
+            this.setState({
+                filteredEmployees: sorteds,
+                order: "asc"
             })
         }
+    }
+    //when input is changing it will dynamically show the associates names that match in the screen
+    handleInputChange = event => {
+        const employees = this.state.employees;
+        const UserInput = event.target.value;
+        const filteredEmployees = employees.filter(employee => employee.name.first.toLowerCase().indexOf(UserInput.toLowerCase()) > -1
+        )
         this.setState({
-            alphabetical: !this.state.alphabetical,
-            sortedEmployees: sortEmp
+            //change the state of filteredEmployes now it holds all the employes that matches users
+            //search and will be passed down in this state
 
-        })
+            filteredEmployees,
+        });
+    };
+
+
+    //API call triggered when page it's refreshed and  when application it's loaded 
+    employeeSearch = () => {
+        API.getUsers()
+            .then(res => this.setState({
+
+                //change their both states to hold all the data from the API call(all employess) and will be passed down trough props like that
+                //employee will remain the same and filteredEmployes will be changed and passed down during application's life. Employee will always hold all employess.
+                filteredEmployees: res.data.results,
+                employees: res.data.results
+            }))
+            .catch(err => console.log(err))
     }
 
-    sortAge = () => {
-        let sortEmp = [];
-        if (this.state.ascending) {
-            sortEmp = this.props.empList.sort((a, b) => {
-                var nameA = a.dob.age, nameB = b.dob.age;
-                if (nameA < nameB)
-                    return -1
-                if (nameA > nameB)
-                    return 1
-                return 0
-            })
-        } else {
-            sortEmp = this.props.empList.sort((a, b) => {
-                var nameA = a.dob.age, nameB = b.dob.age;
-                if (nameA > nameB)
-                    return -1
-                if (nameA < nameB)
-                    return 1
-                return 0
-            })
+    //when button search it's clicked
+    handleSearch = event => {
+        event.preventDefault();
+        if (!this.state.search) {
+            alert("Enter a name")
         }
-        this.setState({
-            ascending: !this.state.ascending,
-            sortedEmployees: sortEmp
+        const { employees, search } = this.state;
 
-        })
+        //filters the object looking for the value that matches the value entered in the input box by the user  (search.this.state)
+        const filteredEmployees = employees.filter(employee => employee.name.first.toLowerCase().includes(search.toLowerCase()));
+
+        this.setState({
+            filteredEmployees
+        });
     }
 
     render() {
         return (
-
             <div>
-                <div className="header">
-                    <div>Photo</div>
-                    <div><p onClick={this.sortName} className="name">Name</p> </div>
-                    <div>Gender</div>
-                    <div><p onClick={this.sortAge} className="age">Age</p></div>
-                    <div>Phone</div>
-                    <div>E-mail</div>
-                </div>
-
-                {
-                    this.state.sortedEmployees.length > 0 &&
-                    this.state.sortedEmployees.map((item, index) => (
-
-                            <EmployeeInfo
-                                image={item.picture.large}
-                                first={item.name.first}
-                                last={item.name.last}
-                                title={item.name.title}
-                                gender={item.gender}
-                                age={item.dob.age}
-                                phone={item.cell}
-                                email={item.email}
-                            />
-                    ))
-                }
-            </div>
-        );
+                <EmployeeSearch
+                    employee={this.state.employees}
+                    handleSearch={this.handleSearch}
+                    handleInputChange={this.handleInputChange} />
+                <EmployeeInfo results={this.state.filteredEmployees}
+                    sortByName={this.sortByName}
+                />
+            </div >
+        )
     }
 }
 
-export default EmployeeResults;
+export default EmployeeResults
